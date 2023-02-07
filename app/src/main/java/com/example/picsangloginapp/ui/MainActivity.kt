643b95319dd7ca5a1cc1d01372b1282d9a318cd2
Eganin <egorzaharin12@gmail.com>
2023-02-07 11:53:04 +0500

    added menu navigation

--- app/src/main/java/com/example/picsangloginapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/picsangloginapp/ui/MainActivity.kt
@@ -18,48 +18,6 @@ class MainActivity : AppCompatActivity(), NavigationProvider {
 
     private lateinit var binding: ActivityMainBinding
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        val picsFragment = if (savedInstanceState == null) PicsFragment()
-//        else supportFragmentManager.findFragmentByTag(PICS_FRAGMENT_TAG)!!
-//
-//        val loginFragment = if (savedInstanceState == null) LoginFragment()
-//        else supportFragmentManager.findFragmentByTag(LOGIN_FRAGMENT_TAG)!!
-//
-//        if (savedInstanceState == null)
-//            supportFragmentManager.beginTransaction().apply {
-//                add(R.id.container, loginFragment, LOGIN_FRAGMENT_TAG)
-//                hide(loginFragment)
-//                add(R.id.container, picsFragment, PICS_FRAGMENT_TAG)
-//                commit()
-//            }
-//        else
-//            binding.navView.selectedItemId =
-//                if (savedInstanceState.getBoolean(IS_LOGIN_SELECTED_KEY)) R.id.navigation_login
-//                else R.id.navigation_pics
-//
-//        binding.navView.setOnItemSelectedListener {
-//            val showPics = it.itemId == R.id.navigation_pics
-//            supportFragmentManager.beginTransaction().apply {
-//                hide(if (showPics) loginFragment else picsFragment)
-//                show(if (showPics) picsFragment else loginFragment)
-//                commit()
-//            }
-//            true
-//        }
-//    }
-//
-//    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-//        super.onSaveInstanceState(outState, outPersistentState)
-//        outState.putBoolean(
-//            IS_LOGIN_SELECTED_KEY,
-//            binding.navView.selectedItemId == R.id.navigation_login
-//        )
-//    }
-
     private val navController: NavController
         get() = findNavController(R.id.nav_host_fragment)
 
@@ -67,7 +25,6 @@ class MainActivity : AppCompatActivity(), NavigationProvider {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.navView.setupWithNavController(navController)
     }
 
     override fun launch(navCommand: NavCommand) {
@@ -110,11 +67,5 @@ class MainActivity : AppCompatActivity(), NavigationProvider {
         }
 
         navController.navigate(uri, navOptions)
-    }
-
-    private companion object {
-        const val PICS_FRAGMENT_TAG = "picsFragment"
-        const val LOGIN_FRAGMENT_TAG = "loginFragment"
-        const val IS_LOGIN_SELECTED_KEY = "isLoginSelected"
     }
 }
---
+++ app/src/main/java/com/example/picsangloginapp/ui/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.example.picsangloginapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.picsangloginapp.R
+import com.example.picsangloginapp.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController =
+            (childFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment).navController
+        binding.mainBottomNavigationView.setupWithNavController(navController)
+    }
+}
